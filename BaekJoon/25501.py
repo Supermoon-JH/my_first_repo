a010e2ec2ebfc4b161e5ac91ec5ac1962d3a7f9e
Supermoon-JH <dknl133@gmail.com>
2023-02-26 22:41:09 +0900

    problem done

---
+++ BaekJoon/25501.py
@@ -0,0 +1,23 @@
+n = int(input())
+cnt = 0
+
+
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r:
+        return 1
+    elif s[l] != s[r]:
+        return 0
+    else:
+        return recursion(s, l+1, r-1)
+
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+
+for i in range(n):
+    s = input()
+    cnt = 0
+    print(isPalindrome(s), cnt)
