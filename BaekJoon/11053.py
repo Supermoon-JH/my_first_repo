592f5cc2fbb291703dcf551ae58c199366745b72
Supermoon-JH <dknl133@gmail.com>
2024-01-22 17:23:06 +0900

    problem done

---
+++ BaekJoon/11053.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+l = list(map(int, input().split()))
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if l[i] > l[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
