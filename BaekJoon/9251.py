ab210a0066b4b7f2132d4ce23182ecbb280417c4
Supermoon-JH <dknl133@gmail.com>
2024-02-01 14:07:56 +0900

    problem done

---
+++ BaekJoon/9251.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a = input().rstrip()
+b = input().rstrip()
+
+dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[len(a)][len(b)])
