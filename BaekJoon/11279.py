15a182d9e9439f8c4a6b62a621333207e8c024e6
Supermoon-JH <dknl133@gmail.com>
2024-02-18 15:49:20 +0900

    problem done

---
+++ BaekJoon/11279.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    num = int(input())
+    if num != 0:
+        heapq.heappush(heap, (-num, num))
+    else:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
