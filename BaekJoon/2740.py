23285705588aa38c7494fd191d78724f19f71ff5
Supermoon-JH <dknl133@gmail.com>
2024-04-14 16:12:23 +0900

    problem done

---
+++ BaekJoon/2740.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+A = []
+for _ in range(N):
+    A.append(list(map(int, input().split())))
+
+M, K = map(int, input().split())
+
+B = []
+for _ in range(M):
+    B.append(list(map(int, input().split())))
+
+C = [[0 for _ in range(K)] for _ in range(N)]
+
+for n in range(N):
+    for k in range(K):
+        for m in range(M):
+            C[n][k] += A[n][m] * B[m][k]
+
+for i in range(len(C)):
+    print(*C[i])
